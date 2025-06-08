d24e52b7f5de31b2d573d09088cb94d6e55a2f57
Guilherme Ciole <guilhermeciole@gmail.com>
2025-06-08 06:30:12 -0300

    fix(config): Update image configuration to use remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,16 @@ const nextConfig = {
     styledComponents: true,
   },
   images: {
-    domains: ['images.unsplash.com', 'source.unsplash.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+      },
+    ],
   },
 };
 
